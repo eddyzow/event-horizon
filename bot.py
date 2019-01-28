8eddd856b961b4953a2bab2207559be94f5b57b7
thefruityeddy <45926119+thefruityeddy@users.noreply.github.com>
2019-01-28 16:42:22 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,83 +16,104 @@ authorizedAuthors = ['christmasy eddyzow#9988','Kyle!#2949']
 points = 123
 repAdder2 = 0
 @client.event
-async def on_message(message):
-    global repAdder
-    global points
-    global authorizedAuthors
-    global user_search_value
-    global count
-    global line
-    global words
-    global repAdder2
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!rep add'):
-        param, repAdder = message.content.split("rep add ",1)
-        if "<@!"+message.author.id+">" != repAdder and "<@"+message.author.id+">" != repAdder:
-            f = open("repubotdata.txt","a")
-            f.write(' '+repAdder+' ',)
-            f.close()
-            user_search_value = str(repAdder)
-            count = 0    
-            with open("repubotdata.txt", 'r') as f:
-                for line in f.readlines():
-                    words = line.lower().split()
-                    print(words)
-                    for word in words:
-                        if word == user_search_value:
-                            count += 1
-                    msg = str(message.author)+', given '+repAdder+' **1** reputation point. They now have '+str(count)+' points'
-                    await client.send_message(message.channel, msg)
-        else:
-            msg = repAdder+" Sorry, you cannot give yourself points!"
-            await client.send_message(message.channel, msg)
-    if message.content.startswith('!rep rem'):
-        param, repAdder = message.content.split("rep rem ",1)
-        if "<@!"+message.author.id+">" != repAdder:
-            user_search_value = str(repAdder)
-            count = 0    
-            with open("repubotdata.txt", 'r') as f:
-                 for line in f.readlines():
-                    words = line.lower().split()
-                    print(words)
-                    print(user_search_value)
-                    words.remove(user_search_value)
-                    words = ' '.join(words)
-                    print(words)
-            f = open("repubotdata.txt","w")
-            f.write(str(words))
-            f.close()
-            with open("repubotdata.txt", 'r') as f:
-                for line in f.readlines():
-                    words = line.lower().split()
-                    print(words)
-                    for word in words:
-                        if word == user_search_value:
-                            count += 1
-                    msg = str(message.author)+', taken **1** reputation point from '+repAdder+'. They now have '+str(count)+' points'
-                    await client.send_message(message.channel, msg)
-        else:
-            msg = repAdder+" You cannot take points from yourself!"
-            await client.send_message(message.channel, msg)
-    if message.content.startswith('!staff ra'):
-        if message.author in authorizedAuthors:
-            param, repAdder = message.content.split("staff ra ",1)
-            print(repAdder)
-    if message.content.startswith('!points'):
-        param, repAdder = message.content.split("points ",1)
+async def on_member_join(member):
+    now = (str(datetime.now()))
+    now1 =(str(member.created_at))
+    now = now.split(' ')
+    print(now)
+    now1 = now1.split(' ')
+    print(now1)
+    now = int(str(now[0]).replace('-', ''))
+    now1 = int(str(now1[0]).replace('-', ''))
+    print(now-now1)
+    dayDiff = now-now1
+    if dayDiff != 0 and dayDiff != 1 and dayDiff != 2 and dayDiff != 2 and dayDiff != 3 and dayDiff != 4 and dayDiff != 5 and dayDiff != 6 and dayDiff != 7 and dayDiff != 8 and dayDiff != 9 and dayDiff != 10 and dayDiff != 11 and dayDiff != 12 and dayDiff != 13 and dayDiff != 14 and dayDiff != 73 and dayDiff != 74 and dayDiff != 75 and dayDiff != 76 and dayDiff != 77 and dayDiff != 78 and dayDiff != 79 and dayDiff != 80 and dayDiff != 81 and dayDiff != 82 and dayDiff != 83 and dayDiff != 84 and dayDiff != 85 and dayDiff != 86 and dayDiff != 87 and dayDiff != 88 and dayDiff != 89 and dayDiff != 8883 and dayDiff != 8882 and dayDiff != 8881 and dayDiff != 8880 and dayDiff != 8879 and dayDiff != 8878 and dayDiff != 8877 and dayDiff != 8876 and dayDiff != 8875 and dayDiff != 8874 and dayDiff != 8873 and dayDiff != 8872 and dayDiff != 8871 and dayDiff != 8870:
+        print(str(member)+' just joined!')
+        repAdder = '<@'+str(member.id)+'>'
         user_search_value = str(repAdder)
-        count = 0    
+        count = 0
         with open("repubotdata.txt", 'r') as f:
             for line in f.readlines():
                 words = line.lower().split()
                 for word in words:
                     if word == user_search_value:
                         count += 1
-                msg = 'This user has '+str(count)+' points'
-                await client.send_message(message.channel, msg)
+                    if word == "-" + str(user_search_value):
+                        count -= 1
+                word = word.replace('!', '')
+                print(count)
+        user = member
+        if count > 19:
+            role = discord.utils.get(user.server.roles, name='Trusted')
+            await client.add_roles(user, role)
+        if count < 20:
+            role = discord.utils.get(user.server.roles, name='Trusted')
+            await client.remove_roles(user, role)
+        if count < 0:
+            role = discord.utils.get(user.server.roles, name='Negative Rep')
+            await client.add_roles(user, role)
+        if count > -1:
+            role = discord.utils.get(user.server.roles, name='Negative Rep')
+            await client.remove_roles(user, role)
+        if count < -14:
+            role = discord.utils.get(user.server.roles, name='Troll (-15 Points)')
+            await client.add_roles(user, role)
+            role = discord.utils.get(user.server.roles, name='Member')
+            await client.remove_roles(user, role)
+        if count > -15:
+            role = discord.utils.get(user.server.roles, name='Troll (-15 Points)')
+            await client.remove_roles(user, role)
+        if count > 49:
+            role = discord.utils.get(user.server.roles, name='Legend')
+            await client.add_roles(user, role)
+        if count < 50:
+            role = discord.utils.get(user.server.roles, name='Legend')
+            await client.remove_roles(user, role)
+        if count > 29:
+            role = discord.utils.get(user.server.roles, name='Master')
+            await client.add_roles(user, role)
+        if count < 30:
+            role = discord.utils.get(user.server.roles, name='Master')
+            await client.remove_roles(user, role)
+        if count > 19:
+            role = discord.utils.get(user.server.roles, name='Trusted')
+            await client.add_roles(user, role)
+        if count < 20:
+            role = discord.utils.get(user.server.roles, name='Trusted')
+            await client.remove_roles(user, role)
+        if count < 0:
+            role = discord.utils.get(user.server.roles, name='Negative Rep')
+            await client.add_roles(user, role)
+        if count > -1:
+            role = discord.utils.get(user.server.roles, name='Negative Rep')
+            await client.remove_roles(user, role)
+        if count < -14:
+            role = discord.utils.get(user.server.roles, name='Troll (-15 Points)')
+            await client.add_roles(user, role)
+            role = discord.utils.get(user.server.roles, name='Member')
+            await client.remove_roles(user, role)
+        if count > -15:
+            role = discord.utils.get(user.server.roles, name='Troll (-15 Points)')
+            await client.remove_roles(user, role)
+        if count > 49:
+            role = discord.utils.get(user.server.roles, name='Legend')
+            await client.add_roles(user, role)
+        if count < 50:
+            role = discord.utils.get(user.server.roles, name='Legend')
+            await client.remove_roles(user, role)
+        if count > 29:
+            role = discord.utils.get(user.server.roles, name='Master')
+            await client.add_roles(user, role)
+        if count < 30:
+            role = discord.utils.get(user.server.roles, name='Master')
+            await client.remove_roles(user, role)
+    else:
+        banner = member
+        try:
+            await client.send_message(member, 'Your account has been detected to be an alt. If this is in error please wait a couple days and try to join again.')
+        except:
+            pass
+        await client.kick(banner)
 
     
         
